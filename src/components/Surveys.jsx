3e42767821a5aa6d8ef19dd29ee6953a1c445726
Trishan Wagle <mailtotrishan@gmail.com>
2023-10-19 21:52:37 +0545

    feat: implemented lazy loading

--- src/components/Surveys.jsx
+++ src/components/Surveys.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import { useSurveysData } from "@/hooks";
 import { ArrowDownOnSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
-import CopyIcon from "./icon/CopyIcon";
+import CopyIcon from "./Icons/CopyIcon";
 
 const Surveys = () => {
   const { surveys, isLoading } = useSurveysData();
@@ -17,13 +16,13 @@ const Surveys = () => {
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="flow-root mt-8">
             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div className="inline-block min-w-full py-2 align-middle sm:pr-6 lg:pr-8 pl-2">
+              <div className="inline-block min-w-full py-2 pl-2 align-middle sm:pr-6 lg:pr-8">
                 <table className="min-w-full divide-y divide-gray-300">
                   <tbody className="divide-y divide-gray-200">
                     {!isLoading &&
                       surveys &&
                       surveys.map((data) => (
-                        <tr className="flex justify-between">
+                        <tr key={data.name} className="flex justify-between">
                           <td className="flex flex-col gap-2 py-[22px] pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-0">
                             <p className="text-base font-semibold">
                               {data.name}
@@ -45,18 +44,18 @@ const Surveys = () => {
                           </td>
 
                           <td className="flex flex-col items-center justify-center gap-2 px-3 py-[22px] text-sm text-gray-500 whitespace-nowrap">
-                            <p className="text-light-text-secondary  font-semibold">
+                            <p className="font-semibold text-light-text-secondary">
                               Expire on
                             </p>
 
-                            <p className="text-base text-brand-black  font-semibold">
+                            <p className="text-base font-semibold text-brand-black">
                               11th Oct, 2023
                             </p>
                           </td>
 
                           <td className="flex items-center gap-3 px-3 py-[22px] text-sm text-gray-500 whitespace-nowrap">
                             <a href="/my-campaigns/7846d41d-1b9f-4c39-8d50-4042a3f3db7f">
-                              <button className="bg-white px-4 btn-filled-white flex items-center gap-2  h-12 justify-center font-semibold bg-brand-white text-light-text-primary rounded-md border border-light-border disabled:opacity-50">
+                              <button className="flex items-center justify-center h-12 gap-2 px-4 font-semibold bg-white border rounded-md btn-filled-white bg-brand-white text-light-text-primary border-light-border disabled:opacity-50">
                                 Dashboard
                               </button>
                             </a>
