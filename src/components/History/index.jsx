9d0603a524ef090207faa514acae147383e668b0
Nischay Maharjan <nischaymaharjann@gmail.com>
2023-12-02 21:50:52 +0545

    fix:History Page UI edit

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -1,26 +1,45 @@
-import ActiveSurveys from "./ActiveSurveys";
-import ExpiredSurveys from "./ExpiredSurveys";
-
-const History = ({ surveys, status, message, date, type }) => {
+import { Link } from "react-router-dom";
+const History = ({ surveys, status, message, date }) => {
   return (
-    <div className="mt-2">
-      {status == "Active" && (type == "default" || type == "active") && (
-        <ActiveSurveys
-          surveys={surveys}
-          status={status}
-          message={message}
-          date={date}
-        />
-      )}
+    <div className="mt-1">
+      <div>
+        {/* <p className="pt-4 font-bold ">{status}</p> */}
 
-      {status == "Expired" && (type == "default" || type == "expired") && (
-        <ExpiredSurveys
-          surveys={surveys}
-          status={status}
-          message={message}
-          date={date}
-        />
-      )}
+        <div className="grid w-full grid-cols-1 mt-1 lg:grid-cols-2 xl:grid-cols-1 text-base">
+          {surveys.map((data) => (
+            <div key={data.id}>
+              <div key={data.id} className="pb-4 mb-2 px-4 bg-white rounded-md">
+                <Link to={`/dashboard/survey/${data.id}`}>
+                  <div className="flex justify-between mt-5 pt-5 text-sm">
+                    <td className="w-[25%]">
+                      <tr>Survey Name</tr>
+                      <tr className="font-bold text-base">{data.name}</tr>
+                    </td>
+                    <td className="w-[25%]">
+                      <tr>Participants</tr>
+                      <tr className="font-bold text-base">
+                        <p className="pl-8">{data.participants.length}</p>
+                      </tr>
+                    </td>
+                    <td className="w-[25%]">
+                      <tr>Expired Date</tr>
+                      <tr className="font-bold text-base">{data.expiry}</tr>
+                    </td>
+                    <td>
+                      <tr>
+                        <p className="pl-5">Status</p>
+                      </tr>
+                      <tr className="text-error font-bold text-base">
+                        {data.status}
+                      </tr>
+                    </td>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -5,67 +5,25 @@ import { useHistory } from "@/hooks";
 const HistoryPage = () => {
   const { surveys } = useHistory();
   const [allSurveys, setAllSurveys] = useState([]);
-  const [typeOfSurveys, setTypeOfSurveys] = useState("default");
 
   useEffect(() => {
     setAllSurveys(surveys);
   }, [surveys]);
 
-  const activeSurveys = allSurveys.filter((data) => {
-    return data.status == "ACTIVE";
-  });
-
   const expiredSurveys = allSurveys.filter((data) => {
     return data.status == "EXPIRED";
   });
+  console.log(expiredSurveys);
 
-  const onFilterChange = (e) => {
-    e.preventDefault();
-
-    const value = e.target.value;
-
-    if (value == "active") {
-      setTypeOfSurveys("active");
-    } else if (value == "expired") {
-      setTypeOfSurveys("expired");
-    } else {
-      setTypeOfSurveys("default");
-    }
-  };
   return (
     <div className="w-full">
-      <div className="flex justify-between">
-        <p className="text-xl font-bold">History</p>
-
-        <select
-          className="rounded-lg"
-          name="selectSurveys"
-          onChange={onFilterChange}
-        >
-          <option value="allSurveys">All Surveys</option>
-
-          <option value="active">Active</option>
-
-          <option value="expired">Expired</option>
-        </select>
-      </div>
-
-      {activeSurveys.length > 0 && (
-        <History
-          surveys={activeSurveys}
-          status="Active"
-          message="You created a survey"
-          date="2023-11-16"
-          type={typeOfSurveys}
-        />
-      )}
+      <div className="flex justify-between font-bold">History</div>
 
       {expiredSurveys.length > 0 && (
         <History
           surveys={expiredSurveys}
           status="Expired"
-          message="Survey has been expired"
-          type={typeOfSurveys}
+          message="This survey has been expired"
         />
       )}
     </div>
