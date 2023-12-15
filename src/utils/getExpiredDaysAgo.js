a03b8076838e9aa76458b4e9b0a00c7f2fe98a1a
Nischay Maharjan <nischaymaharjann@gmail.com>
2023-12-15 10:58:22 +0545

    feat:Added the Filter button and Faqs

---
+++ src/utils/getExpiredDaysAgo.js
@@ -0,0 +1,15 @@
+import { filter } from "lodash";
+import React from "react";
+
+const getExpiredDaysAgo = (expiredSurveysData, days) => {
+  const filterValue = expiredSurveysData.filter((data) => {
+    const date = new Date(data.expiry);
+    const today = new Date();
+
+    if (new Date(today - 86400000 * days) <= date) {
+      return data;
+    }
+  });
+  return filterValue;
+};
+export default getExpiredDaysAgo;
