27813dace64415c2599ab2ccb7acbc151047aa4a
Nischay Maharjan <nischaymaharjann@gmail.com>
2024-02-06 17:55:59 +0545

    feat: all the fields are added

---
+++ super-admin/components/shared/dynamic-form-field.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Fragment } from "react";
+import { useFieldArray } from "react-hook-form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { PlusCircle, Trash2 } from "lucide-react";
+
+type DynamicFormFieldProps = {
+  form: any;
+  name: string;
+  label: string;
+};
+
+const DynamicFormField = ({ form, name, label }: DynamicFormFieldProps) => {
+  const { fields, append, remove } = useFieldArray({
+    name,
+    control: form.control,
+    rules: {
+      required: `Please add at least 1 ${name}`,
+    },
+  });
+  return (
+    <div className="flex flex-col gap-2">
+      <Label className="font-bold">{label}</Label>
+      {fields.length > 0 &&
+        fields.map((field, index) => (
+          <div key={field.id}>
+            <div className="w-full">
+              <Label className="text-sm">
+                {label} {index + 1}
+              </Label>
+
+              <div className="flex items-center w-full mt-2">
+                <Input
+                  className="w-full"
+                  required
+                  {...form.register(`${name}.${index}.name`)}
+                />
+
+                {index > 0 && (
+                  <Button
+                    onClick={() => remove(index)}
+                    type="button"
+                    size="sm"
+                    className="ml-4"
+                  >
+                    <Trash2 className="w-5 h-5" />
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+
+      <Button
+        onClick={() =>
+          append({
+            name: "",
+          })
+        }
+        type="button"
+        className="w-full"
+        variant="secondary"
+      >
+        <PlusCircle className="w-4 h-4 mr-2" />
+
+        <p>Add {name}</p>
+      </Button>
+    </div>
+  );
+};
+
+export default DynamicFormField;
