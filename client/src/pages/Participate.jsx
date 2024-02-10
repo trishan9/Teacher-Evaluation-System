05c7e3243c343c6b50651e744fd47533da720800
Trishan Wagle <mailtotrishan@gmail.com>
2024-02-10 16:20:52 +0545

    fix: build error

--- client/src/pages/Participate.jsx
+++ client/src/pages/Participate.jsx
@@ -1,5 +1,5 @@
 import { useParams, useSearchParams, useNavigate } from 'react-router-dom';
-import { useSurveyAndSchool } from '@/hooks';
+// import { useSurveyAndSchool } from '@/hooks';
 import StepOne from '@/components/Participate/Form/StepOne';
 import StepTwo from '@/components/Participate/Form/StepTwo';
 import StepThree from '@/components/Participate/Form/StepThree';
@@ -12,7 +12,7 @@ const Participate = () => {
     const navigate = useNavigate()
     const step = parseInt(searchParams.get("step"))
 
-    const { schoolData, survey, isLoading } = useSurveyAndSchool(id)
+    // const { schoolData, survey, isLoading } = useSurveyAndSchool(id)
 
     const handleNextStep = (values) => {
         localStorage.setItem(`step${step ? step : 1}`, JSON.stringify(values))
@@ -23,44 +23,47 @@ const Participate = () => {
         navigate(`/participate/${id}?step=${step - 1}`)
     }
 
+    // return (
+    //     <div>
+    //         {isLoading && <p className='p-6'>Loading....</p>}
+
+    //         {!schoolData?.logo && <p className='p-6'>Loading....</p>}
+
+    //         {!isLoading && schoolData?.logo && survey && (
+    //             <div className='sm:p-6 mt-2 bg-white rounded-lg sm:min-h-[90vh] w-full'>
+    //                 <div className='flex flex-col items-center justify-center w-full gap-6'>
+    //                     <img src={schoolData.logo} />
+
+    //                     <p className='text-xl font-semibold capitalize sm:text-3xl'>{survey.name} Survey</p>
+
+    //                     {!step &&
+    //                         <StepOne handleNextStep={handleNextStep} schoolData={schoolData} />
+    //                     }
+
+    //                     {step == 1 &&
+    //                         <StepOne handleNextStep={handleNextStep} schoolData={schoolData} />
+    //                     }
+
+    //                     {step == 2 &&
+    //                         <StepTwo handleNextStep={handleNextStep} handlePreviousStep={handlePreviousStep} survey={survey} schoolData={schoolData} />
+    //                     }
+
+    //                     {step == 3 &&
+    //                         <StepThree handleNextStep={handleNextStep} handlePreviousStep={handlePreviousStep} survey={survey} />
+    //                     }
+
+    //                     {step == 4 &&
+    //                         <StepFour handlePreviousStep={handlePreviousStep} survey={survey} id={id} />
+    //                     }
+    //                 </div>
+    //             </div>
+    //         )}
+
+    //         {!isLoading && schoolData?.logo && survey && <Footer />}
+    //     </div>
+    // )
     return (
-        <div>
-            {isLoading && <p className='p-6'>Loading....</p>}
-
-            {!schoolData?.logo && <p className='p-6'>Loading....</p>}
-
-            {!isLoading && schoolData?.logo && survey && (
-                <div className='sm:p-6 mt-2 bg-white rounded-lg sm:min-h-[90vh] w-full'>
-                    <div className='flex flex-col items-center justify-center w-full gap-6'>
-                        <img src={schoolData.logo} />
-
-                        <p className='text-xl font-semibold capitalize sm:text-3xl'>{survey.name} Survey</p>
-
-                        {!step &&
-                            <StepOne handleNextStep={handleNextStep} schoolData={schoolData} />
-                        }
-
-                        {step == 1 &&
-                            <StepOne handleNextStep={handleNextStep} schoolData={schoolData} />
-                        }
-
-                        {step == 2 &&
-                            <StepTwo handleNextStep={handleNextStep} handlePreviousStep={handlePreviousStep} survey={survey} schoolData={schoolData} />
-                        }
-
-                        {step == 3 &&
-                            <StepThree handleNextStep={handleNextStep} handlePreviousStep={handlePreviousStep} survey={survey} />
-                        }
-
-                        {step == 4 &&
-                            <StepFour handlePreviousStep={handlePreviousStep} survey={survey} id={id} />
-                        }
-                    </div>
-                </div>
-            )}
-
-            {!isLoading && schoolData?.logo && survey && <Footer />}
-        </div>
+        <div>Nothing</div>
     )
 }
 
